4e957670a2759f8da3ae2a705ec2299e07c809f8
DumeD <daviddume3@gmail.com>
2017-01-01 17:53:10 +0100

    displayed weather

--- src/containers/weather.js
+++ src/containers/weather.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import _ from 'lodash';
 
 class Weather extends Component {
+  constructor(props) {
+    super(props);
+    this.convert = this.convert.bind(this);
+  }
+  convert(temp) {
+    return _.round(temp - 273.15);
+  }
   render() {
 
     if(!this.props.weather) {
       return <div></div>;
     }
+
     var icon = this.props.weather.weather[0].icon;
+
     return (
       <div className='container'>
-        <h3>City Name</h3>
-        <div>
+        <h3>{this.props.weather.name}</h3>
+        <div className='weather'>
+          <p>{this.props.weather.weather[0].main}</p>
           <img src={`http://openweathermap.org/img/w/${icon}.png`} className='weather-img'/>
         </div>
-        <div>
-          <p>Temperature: {this.props.weather.main.temp}</p>
+        <div className='temperatures'>
+          <p>Max: {this.convert(this.props.weather.main.temp_max)}° C</p>
+          <p>Temperature: {this.convert(this.props.weather.main.temp)}° C</p>
+          <p>Min: {this.convert(this.props.weather.main.temp_min)}° C</p>
         </div>
       </div>
     );
