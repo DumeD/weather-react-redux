cf9fa3571851b8be1254c469c32b75a629f2405d
DumeD <daviddume3@gmail.com>
2017-01-01 17:42:17 +0100

    added icons

--- src/containers/weather.js
+++ src/containers/weather.js
@@ -3,8 +3,20 @@ import { connect } from 'react-redux';
 
 class Weather extends Component {
   render() {
+
+    if(!this.props.weather) {
+      return <div></div>;
+    }
+    var icon = this.props.weather.weather[0].icon;
     return (
-      <div>
+      <div className='container'>
+        <h3>City Name</h3>
+        <div>
+          <img src={`http://openweathermap.org/img/w/${icon}.png`} className='weather-img'/>
+        </div>
+        <div>
+          <p>Temperature: {this.props.weather.main.temp}</p>
+        </div>
       </div>
     );
   }
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -3,8 +3,9 @@ import { FETCH_WEATHER } from '../actions/index';
 export default function(state = null, action) {
   switch (action.type) {
     case FETCH_WEATHER:
-      return action.payload.data;
       console.log(action.payload.data);
+      return action.payload.data;
+
     }
   return state;
 }
